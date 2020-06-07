f88ebaeb3a8beb986dfd65f75cb4c5ca77b8222f
Jordy <jordy.mous@brightest.be>
2020-06-07 20:05:23 +0200

    Test2

--- src/test/java/framework/BaseTest.java
+++ src/test/java/framework/BaseTest.java
@@ -21,13 +21,9 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.io.FileHandler;
-
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 
 public class BaseTest {
 
